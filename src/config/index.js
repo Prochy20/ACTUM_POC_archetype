2ad252fbdba443a65d0e2cdb57452fdd920eb9bc
Prochy20 <prochy.20.m@seznam.cz>
2022-04-05 12:07:31 +0200

    setup config

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,23 @@ const dontenv = require('dotenv');
 dontenv.config();
 
 module.exports = {
-    ...process.env,
+    /**
+     * @property {Object} API ENV Constants
+     */
+    API: {
+        /**
+         * @property {Number} PORT Port which should API listen on
+         */
+        PORT: Number(process.env.API_PORT) || 8055,
+    },
+
+    /**
+     * @property {Object} APP Environment Constants
+     */
+    ENV: {
+        /**
+         * @property {String} NODE_ENV
+         */
+        NODE_ENV: process.env.NODE_ENV,
+    },
 };
