45cd3fed983834c63931cffbde9f921280ec6d4f
Prochy20 <prochy.20.m@seznam.cz>
2022-04-05 15:00:57 +0200

    add routing

--- src/service.js
+++ src/service.js
@@ -4,10 +4,15 @@ const cors = require('cors');
 
 const config = require('./config');
 const logger = require('./logger');
+const api = require('./api');
 
 const app = express();
+
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(api);
 
 app.listen(config.API.PORT, () => logger.info(
     `Microservice started - listening on ${config.API.PORT}`,
